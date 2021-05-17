457aee68ea599b525b85703abec7648c52868da0
Nick Carver <nicholascarver2@outlook.com>
2021-05-17 11:31:24 -0500

    created fixture data

--- rareapi/models/RareUser.py
+++ rareapi/models/RareUser.py
@@ -7,5 +7,4 @@ class RareUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=50)
     created_on = models.DateField(default=timezone.now())
-    active = models.BooleanField()
     profile_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
