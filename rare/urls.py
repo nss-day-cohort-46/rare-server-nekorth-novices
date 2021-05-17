0aabcb8a277bb4a4c9dfdbd9adf2c313ad9f9311
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-17 14:02:16 -0500

    Added CategoryViewSet and create function; registered route in urls.py

--- rare/urls.py
+++ rare/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rareapi.views import CategoryViewSet
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryViewSet, 'category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
---
+++ rareapi/views/__init__.py
@@ -0,0 +1,1 @@
+from .CategoryViewSet import CategoryViewSet
