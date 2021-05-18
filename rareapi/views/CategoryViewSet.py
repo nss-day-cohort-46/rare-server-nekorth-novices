de94bd6d3e72eb4243736d382bf5db16899d6f80
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-18 09:00:13 -0500

    Added list function to CategoryViewSet; successfully listed all categories

--- rareapi/views/CategoryViewSet.py
+++ rareapi/views/CategoryViewSet.py
@@ -1,9 +1,9 @@
 from django.http.response import HttpResponse
-from rareapi.models import Category, RareUser
-from django.contrib.auth.models import User
+from rareapi.models import Category
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework import status
 
 class CategoryViewSet(ViewSet):
     def create(self, request):
@@ -13,9 +13,15 @@ class CategoryViewSet(ViewSet):
         try:
             category.save()
             serializer = CategorySerializer(category, context={'request', request})
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception:
             return HttpResponse(Exception)
+    
+    def list(self, request):
+        # TODO: admin-only endpoint
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True, context={'request': request})
+        return Response(serializer.data)
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
