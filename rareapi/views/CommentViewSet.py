21c0aa4565f888f0ea9990e649a6911c4a7dffca
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-18 12:58:30 -0500

    Added destroy function to CommentViewSet

--- rareapi/views/CommentViewSet.py
+++ rareapi/views/CommentViewSet.py
@@ -1,6 +1,6 @@
 from rareapi.models.Post import Post
 from django.http.response import HttpResponse
-from rareapi.models import Category, Comment, RareUser
+from rareapi.models import Comment, RareUser
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
@@ -20,6 +20,16 @@ class CommentViewSet(ViewSet):
             comment.save()
             serializer = CommentSerializer(comment, context={'request', request})
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception:
+            return HttpResponse(Exception)
+
+    def destroy(self, request, pk):
+        # TODO: only the comment creator should be able to delete
+        try:
+            comment = Comment.objects.get(pk=pk)
+            comment.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
         except Exception:
             return HttpResponse(Exception)
 
