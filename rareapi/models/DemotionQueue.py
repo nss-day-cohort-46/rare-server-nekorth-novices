570c842ade2d1eb07f146f5f24fd9fc5b2f7c8d9
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-19 09:15:45 -0500

    Refactored DemotionQueue and and Post models remove foreign key constraint; refactored Category model to put unique constraint on label field

--- rareapi/models/DemotionQueue.py
+++ rareapi/models/DemotionQueue.py
@@ -3,5 +3,5 @@ from django.db import models
 class DemotionQueue(models.Model):
 
     admin = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="admin")
-    approver = models.ForeignKey("RareUser", on_delete=models.DO_NOTHING, null=True, related_name="approver")
+    approver = models.ForeignKey("RareUser", on_delete=models.SET_NULL, null=True, related_name="approver")
     action = models.CharField(max_length=50)
