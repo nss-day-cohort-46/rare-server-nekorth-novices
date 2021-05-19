59f1869622882ee26b2a016d476225df41f942c7
Nick Carver <nicholascarver2@outlook.com>
2021-05-19 13:03:18 -0500

    changed the location of check active and change active

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,6 +1,7 @@
-from .AuthViewSet import login_user, register_user, check_active
+from .AuthViewSet import login_user, register_user
 from .CategoryViewSet import CategoryViewSet
 from .CommentViewSet import CommentViewSet
 from .TagViewSet import TagViewSet
 from .PostViewSet import PostViewSet
 from .RareUserViewSet import RareUserViewSet
+from .PermissionsViewSet import check_active, change_active
